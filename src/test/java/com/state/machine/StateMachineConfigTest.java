e626b32a9bc39c8fb50d67fb10bdc9377a3127e8
JossguerreroP <josguerrerop@udistrital.edu.co>
2025-08-12 16:44:49 -0500

    testing

---
+++ src/test/java/com/state/machine/StateMachineConfigTest.java
@@ -0,0 +1,53 @@
+package com.state.machine;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.UUID;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.config.StateMachineFactory;
+
+import com.state.machine.domain.PaymentEvent;
+import com.state.machine.domain.PaymentState;
+
+
+
+@SpringBootTest
+class StateMachineConfigTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+	
+	@Autowired
+    StateMachineFactory<PaymentState, PaymentEvent> factory;
+
+    @Test
+    void testNewStateMachine() {
+        StateMachine<PaymentState, PaymentEvent> sm = factory.getStateMachine(UUID.randomUUID());
+
+        sm.start();
+
+        System.out.println(sm.getState().toString());
+
+        sm.sendEvent(PaymentEvent.PRE_AUTHORIZE);
+
+        System.out.println(sm.getState().toString());
+
+        sm.sendEvent(PaymentEvent.PRE_AUTH_APPROVED);
+
+        System.out.println(sm.getState().toString());
+
+        sm.sendEvent(PaymentEvent.PRE_AUTH_DECLINED);
+
+        System.out.println(sm.getState().toString());
+
+    }
+	
+	
+}
